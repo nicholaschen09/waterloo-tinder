23bf8913402cf9055d145d2ff8354d563327703a
nicholaschen09 <nicholas.chen243@gmail.com>
2025-03-29 12:52:58 -0400

    fix

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,7 +4,7 @@ import { LoginForm } from '@/components/login-form'
 
 export default function LoginPage() {
   return (
-    <div className='min-h-screen flex flex-col items-center justify-center p-4'>
+    <div className='min-h-screen flex flex-col items-center justify-center p-4 bg-white'>
       <h1 className='text-4xl font-bold mb-8 text-center'>Match</h1>
       <LoginForm />
     </div>
